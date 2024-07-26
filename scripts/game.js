cd52411f5609a928f2aa6454a10b952171788d38
ethancic <cic.ethan@gmail.com>
2024-07-27 06:37:07 +0800

    easeOutCubic

--- scripts/game.js
+++ scripts/game.js
@@ -310,7 +310,7 @@ class Game {
         requestAnimationFrame(animate);
     }
 
-    easeOutCubic(t) {
-        return 1 - Math.pow(1 - t, 3);
+    easeInOutCubic(t) {
+        return t < 0.5 ? 4 * t * t * t : 1 - Math.pow(-2 * t + 2, 3) / 2;
     }
 }
